a07776eca363d98553717d50537723cca2d82438
mengel <mengel@fnal.gov>
2015-09-22 11:32:09 -0500

    now we show who is logged in via shibboleth

--- webservice/poms_service.py
+++ webservice/poms_service.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey, and_, or_, create_engine, null, desc, text
 from datetime import datetime, tzinfo,timedelta
 from jinja2 import Environment, PackageLoader
-from model.poms_model import Service, ServiceDowntime
+from model.poms_model import Service, ServiceDowntime, Experimenter
 
 ZERO = timedelta(0)
 
@@ -66,7 +66,7 @@ class poms_service:
     @cherrypy.expose
     def index(self):
         template = self.jinja_env.get_template('front.html')
-        return template.render(services=self.service_status_hier('All'))
+        return template.render(services=self.service_status_hier('All'),current_experimenter=self.get_current_experimenter())
 
     @cherrypy.expose
     def test(self):
