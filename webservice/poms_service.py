d813465b9c710437940a6ae821218117c6266065
Marc Mengel <mengel@fnal.gov>
2016-02-25 22:57:45 -0600

    launch/kill from campaign screen

--- webservice/poms_service.py
+++ webservice/poms_service.py
@@ -948,6 +948,7 @@ class poms_service:
             res.append('<a href="%s/campaign_sheet?campaign_id=%d&tmax=%s"><i class="external table icon" data-content="Campaign Spreadsheet" data-variation="basic"></i></a>' % ( self.path, c.campaign_id, tmaxs))
             res.append('<a href="%s/campaign_time_bars?campaign_id=%d&tmin=%s&tmax=%s"><i class="external tasks icon" data-content="Tasks in Campaign Time Bars" data-variation="basic"></i></a>' % ( self.path, c.campaign_id, tmins, tmaxs))
             res.append('<a href="%s/campaign_info?campaign_id=%d"><i class="external info circle icon" data-content="Campaign Information" data-variation="basic"></i></a>' % ( self.path, c.campaign_id ))
+            res.append('<a href="%s/launch_jobs?campaign_id=%d"><i class="rocket icon" data-content="Launch Tasks for Campaign" data-variation="basic"></i></a>' % ( self.path, c.campaign_id))
             res.append('<a href="%s/kill_jobs?campaign_id=%d"><i class="trash icon" data-content="Kill jobs in Campaign" data-variation="basic"></i></a>' % ( self.path, c.campaign_id))
             res.append('</td>')
             counts = self.job_counts(tmax = tmax, tmin = tmin, tdays = tdays, campaign_id = c.campaign_id)
