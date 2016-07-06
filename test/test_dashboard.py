ecc113433b502874754420ba961a6e0054910eda
mgheith <mgheith@fnal.gov>
2016-07-06 16:03:04 -0500

    added standing up and tearing down poms to test suite

--- test/test_dashboard.py
+++ test/test_dashboard.py
@@ -1,6 +1,10 @@
 from webclient import WebClient
 import unittest
 import ConfigParser
+import time
+import subprocess
+import sys
+
 
 config = ConfigParser.RawConfigParser()
 config.read('../webservice/poms.ini')
@@ -8,7 +12,7 @@ config.read('../webservice/poms.ini')
 port = config.get('global', 'server.socket_port')
 pomspath = config.get('global', 'pomspath')
 pomspath = pomspath.replace("'", "")
-
+pidpath = config.get('global', 'log.pidfile')[1:-1]
 base_url = "http://localhost:"+port+pomspath+"/"
 
 
@@ -33,6 +37,25 @@ class indexMethods(unittest.TestCase):
 
 
 
+def setUpModule():
+    print "************* SETTING UP POMS *************"
+    try:
+        proc = subprocess.Popen("cd ../ && source /fnal/ups/etc/setups.sh && setup -. poms && cd webservice/ && python service.py &", shell=True)
+    except OSError as e:
+        print >>sys.stderr, "Execution failed:", e
+    time.sleep(10)
+
+
+
+def tearDownModule():
+    print "************* TEARING DOWN POMS *************"
+    with open(pidpath, 'r') as f:
+        pid = f.readline()
+    f.close()
+    try:
+        proc = subprocess.Popen("kill " + pid, shell=True)
+    except OSError as e:
+        print >>sys.stderr, "Excecution failed:", e
 
 
 
