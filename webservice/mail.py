cbda3a2adfbc4ef7976b0eb7ef1fc0b88dea841b
mgheith <mgheith@fnal.gov>
2016-07-25 17:04:40 -0500

    added debug mode to constructor of mail class

--- webservice/mail.py
+++ webservice/mail.py
@@ -3,9 +3,10 @@ from email.mime.text import MIMEText
 
 
 class Mail:
-    def __init__(self):
+    def __init__(self, debug=0):
         self.server = "smtp.fnal.gov:25"
         self.sender = "poms@fnal.gov"
+        self.debug = debug
 
     def send(self, subj, msg, to):
         # Create a text/plain message
@@ -16,6 +17,7 @@ class Mail:
         msg['To'] = to
 
         s = smtplib.SMTP(self.server)
+        s.set_debuglevel(self.debug)
         s.sendmail(self.sender, [to], msg.as_string())
         s.quit()
 
@@ -23,5 +25,5 @@ class Mail:
 
 if __name__ == '__main__':
     #from mail import Mail
-    mail = Mail()
+    mail = Mail(debug=1)  #turn on optional debug mode default is off
     mail.send(subj="my subject", msg="my message", to="mgheith@fnal.gov")
