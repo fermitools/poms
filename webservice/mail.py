b98e43baf68dd561d729cdfeb6f2d95d6c18371a
mgheith <mgheith@fnal.gov>
2016-07-26 13:15:31 -0500

    made mail settings configurable

--- webservice/mail.py
+++ webservice/mail.py
@@ -1,12 +1,19 @@
 import smtplib
 from email.mime.text import MIMEText
+import ConfigParser
 
 
 class Mail:
-    def __init__(self, debug=0):
-        self.server = "smtp.fnal.gov:25"
-        self.sender = "poms@fnal.gov"
-        self.debug = debug
+    def __init__(self):
+        self.server, self.sender, self.debug = self.__get_smtp_info()
+
+    def __get_smtp_info(self):
+        config = ConfigParser.ConfigParser()
+        config.read('../webservice/poms.ini')
+        server = config.get('smtp', 'server')
+        sender = config.get('smtp', 'sender')
+        debug = config.getint('smtp', 'debug')
+        return (server, sender, debug)
 
     def send(self, subj, msg, to):
         # Create a text/plain message
@@ -22,8 +29,7 @@ class Mail:
         s.quit()
 
 
-
 if __name__ == '__main__':
     #from mail import Mail
-    mail = Mail(debug=1)  #turn on optional debug mode default is off
+    mail = Mail()
     mail.send(subj="my subject", msg="my message", to="mgheith@fnal.gov")
