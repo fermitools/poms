001020f3abf984035f3f400347addbda07919e4b
ahandres <ahandres@fnal.gov>
2016-10-14 10:13:43 -0500

    Correcting indentation, use of taps

--- webservice/CampaignsPOMS.py
+++ webservice/CampaignsPOMS.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 '''
-This module contain the methods that allow to create campaigns, definitions and templates. 
-List of methods:  launch_template_edit, campaign_definition_edit, campaign_edit, campaign_edit_query.       
-Author: Felipe Alba ahandresf@gmail.com, This code is just a modify version of functions in poms_service.py written by Marc Mengel, Michael Gueith and Stephen White. 
-Date: September 30, 2016. 
+This module contain the methods that allow to create campaigns, definitions and templates.
+List of methods:  launch_template_edit, campaign_definition_edit, campaign_edit, campaign_edit_query.
+Author: Felipe Alba ahandresf@gmail.com, This code is just a modify version of functions in poms_service.py written by Marc Mengel, Michael Gueith and Stephen White.
+Date: September 30, 2016.
 '''
 
 from model.poms_model import Experiment
@@ -16,164 +16,158 @@ class CampaignsPOMS():
 		data = {}
 		message = None
 		data['exp_selections'] = dbhandle.query(Experiment).filter(~Experiment.experiment.in_(["root","public"])).order_by(Experiment.experiment)
-
 		action = kwargs.pop('action',None)
 		exp = kwargs.pop('experiment',None)
-
 		if action == 'delete':
-		    name = kwargs.pop('name')
-		    try:
-			dbhandle.query(LaunchTemplate).filter(LaunchTemplate.experiment==exp).filter(LaunchTemplate.name==name).delete()
-			dbhandle.commit()
-		    except Exception, e:
-			message = "The launch template, %s, has been used and may not be deleted." % name
-			loghandle(message)
-			loghandle(e.message)
-			dbhandle.rollback()
+			name = kwargs.pop('name')
+			try:
+				dbhandle.query(LaunchTemplate).filter(LaunchTemplate.experiment==exp).filter(LaunchTemplate.name==name).delete()
+				dbhandle.commit()
+			except Exception, e:
+				message = "The launch template, %s, has been used and may not be deleted." % name
+				loghandle(message)
+				loghandle(e.message)
+				dbhandle.rollback()
 
 		if action == 'add' or action == 'edit':
-		    ae_launch_id = kwargs.pop('ae_launch_id')
-		    ae_launch_name = kwargs.pop('ae_launch_name')
-		    ae_launch_host = kwargs.pop('ae_launch_host')
-		    ae_launch_account = kwargs.pop('ae_launch_account')
-		    ae_launch_setup = kwargs.pop('ae_launch_setup')
-		    experimenter_id = kwargs.pop('experimenter_id')
-		    try:
-			if action == 'add':
-			    template = LaunchTemplate(experiment=exp, name=ae_launch_name, launch_host=ae_launch_host, launch_account=ae_launch_account,
-						      launch_setup=ae_launch_setup,creator = experimenter_id, created = datetime.now(utc))
-			    dbhandle.add(template)
+			ae_launch_id = kwargs.pop('ae_launch_id')
+			ae_launch_name = kwargs.pop('ae_launch_name')
+			ae_launch_host = kwargs.pop('ae_launch_host')
+			ae_launch_account = kwargs.pop('ae_launch_account')
+			ae_launch_setup = kwargs.pop('ae_launch_setup')
+			experimenter_id = kwargs.pop('experimenter_id')
+			try:
+				if action == 'add':
+					template = LaunchTemplate(experiment=exp, name=ae_launch_name, launch_host=ae_launch_host, launch_account=ae_launch_account,
+						launch_setup=ae_launch_setup,creator = experimenter_id, created = datetime.now(utc))
+						dbhandle.add(template)
 			else:
-			    columns = {"name":           ae_launch_name,
+				columns = {"name":           ae_launch_name,
 				       "launch_host":    ae_launch_host,
 				       "launch_account": ae_launch_account,
 				       "launch_setup":   ae_launch_setup,
 				       "updated":        datetime.now(utc),
 				       "updater":        experimenter_id
 				       }
-			    template = dbhandle.query(LaunchTemplate).filter(LaunchTemplate.launch_id==ae_launch_id).update(columns)
+				template = dbhandle.query(LaunchTemplate).filter(LaunchTemplate.launch_id==ae_launch_id).update(columns)
 		    except IntegrityError, e:
-			message = "Integrity error - you are most likely using a name which already exists in database."
-			loghandle(e.message)
-			dbhandle.rollback()
-		    except SQLAlchemyError, e:
-			message = "SQLAlchemyError.  Please report this to the administrator.   Message: %s" % e.message
-			loghandle(e.message)
-			dbhandle.rollback()
-		    else:
-			dbhandle.commit()
+				message = "Integrity error - you are most likely using a name which already exists in database."
+				loghandle(e.message)
+				dbhandle.rollback()
+			except SQLAlchemyError, e:
+				message = "SQLAlchemyError.  Please report this to the administrator.   Message: %s" % e.message
+				loghandle(e.message)
+				dbhandle.rollback()
+			else:
+				dbhandle.commit()
 
 		# Find templates
 		if exp: # cuz the default is find
-		    data['curr_experiment'] = exp
-		    data['authorized'] = seshandle('experimenter').is_authorized(exp)
-		    data['templates'] = dbhandle(LaunchTemplate,Experiment).join(Experiment).filter(LaunchTemplate.experiment==exp).order_by(LaunchTemplate.name)
+			data['curr_experiment'] = exp
+			data['authorized'] = seshandle('experimenter').is_authorized(exp)
+			data['templates'] = dbhandle(LaunchTemplate,Experiment).join(Experiment).filter(LaunchTemplate.experiment==exp).order_by(LaunchTemplate.name)
 
 		data['message'] = message
 		return data
-
 
 
 	def campaign_definition_edit(self, dbhandle, loghandle, seshandle, *args, **kwargs):
 		data = {}
 		message = None
 		data['exp_selections'] = dbhandle.query(Experiment).filter(~Experiment.experiment.in_(["root","public"])).order_by(Experiment.experiment)
-
 		action = kwargs.pop('action',None)
 		exp = kwargs.pop('experiment',None)
-
 		if action == 'delete':
-		    name = kwargs.pop('name')
-		    cid = kwargs.pop('campaign_definition_id')
-		    try:
-			dbhandle.query(CampaignRecovery).filter(CampaignRecovery.campaign_definition_id==cid).delete()
-			dbhandle.query(CampaignDefinition).filter(CampaignDefinition.campaign_definition_id==cid).delete()
-			dbhandle.commit()
-		    except Exception, e:
-			message = "The campaign definition, %s, has been used and may not be deleted." % name
-			loghandle(message)
-			loghandle(e.message)
-			loghandle.rollback()
+			name = kwargs.pop('name')
+			cid = kwargs.pop('campaign_definition_id')
+			try:
+				dbhandle.query(CampaignRecovery).filter(CampaignRecovery.campaign_definition_id==cid).delete()
+				dbhandle.query(CampaignDefinition).filter(CampaignDefinition.campaign_definition_id==cid).delete()
+				dbhandle.commit()
+			except Exception, e:
+				message = "The campaign definition, %s, has been used and may not be deleted." % name
+				loghandle(message)
+				loghandle(e.message)
+				loghandle.rollback()
 
 		if action == 'add' or action == 'edit':
-		    campaign_definition_id = kwargs.pop('ae_campaign_definition_id')
-		    name = kwargs.pop('ae_definition_name')
-		    input_files_per_job = kwargs.pop('ae_input_files_per_job')
-		    output_files_per_job = kwargs.pop('ae_output_files_per_job')
-		    output_file_patterns = kwargs.pop('ae_output_file_patterns')
-		    launch_script = kwargs.pop('ae_launch_script')
-		    definition_parameters = kwargs.pop('ae_definition_parameters')
-		    recoveries = kwargs.pop('ae_definition_recovery')
-		    experimenter_id = kwargs.pop('experimenter_id')
-		    try:
-			if action == 'add':
-			    cd = CampaignDefinition( name=name, experiment=exp,
-						    input_files_per_job=input_files_per_job, output_files_per_job = output_files_per_job,
-						    output_file_patterns = output_file_patterns,
-						    launch_script=launch_script, definition_parameters=definition_parameters,
-						    creator=experimenter_id, created=datetime.now(utc))
+			campaign_definition_id = kwargs.pop('ae_campaign_definition_id')
+			name = kwargs.pop('ae_definition_name')
+			input_files_per_job = kwargs.pop('ae_input_files_per_job')
+			output_files_per_job = kwargs.pop('ae_output_files_per_job')
+			output_file_patterns = kwargs.pop('ae_output_file_patterns')
+			launch_script = kwargs.pop('ae_launch_script')
+			definition_parameters = kwargs.pop('ae_definition_parameters')
+			recoveries = kwargs.pop('ae_definition_recovery')
+			experimenter_id = kwargs.pop('experimenter_id')
+			try:
+				if action == 'add':
+					cd = CampaignDefinition( name=name, experiment=exp,
+							input_files_per_job=input_files_per_job, output_files_per_job = output_files_per_job,
+							output_file_patterns = output_file_patterns,
+							launch_script=launch_script, definition_parameters=definition_parameters,
+							creator=experimenter_id, created=datetime.now(utc))
 
-			    dbhandle.add(cd)
-			    dbhandle.flush()
-			    campaign_definition_id = cd.campaign_definition_id
+					dbhandle.add(cd)
+					dbhandle.flush()
+					campaign_definition_id = cd.campaign_definition_id
 			else:
-			    columns = {"name":                  name,
-				       "input_files_per_job":   input_files_per_job,
-				       "output_files_per_job":  output_files_per_job,
-				       "output_file_patterns":  output_file_patterns,
-				       "launch_script":         launch_script,
-				       "definition_parameters": definition_parameters,
-				       "updated":               datetime.now(utc),
-				       "updater":               experimenter_id
-				       }
-			    cd = dbhandle.query(CampaignDefinition).filter(CampaignDefinition.campaign_definition_id==campaign_definition_id).update(columns)
-			
+				columns = {"name":                  name,
+					"input_files_per_job":   input_files_per_job,
+					"output_files_per_job":  output_files_per_job,
+					"output_file_patterns":  output_file_patterns,
+					"launch_script":         launch_script,
+					"definition_parameters": definition_parameters,
+					"updated":               datetime.now(utc),
+					"updater":               experimenter_id
+					}
+					cd = dbhandle.query(CampaignDefinition).filter(CampaignDefinition.campaign_definition_id==campaign_definition_id).update(columns)
+
 			# now fixup recoveries -- clean out existing ones, and
 			# add listed ones.
 			dbhandle.query(CampaignRecovery).filter(CampaignRecovery.campaign_definition_id == campaign_definition_id).delete()
 			i = 0
 			for rtn in json.loads(recoveries):
-			    rt = dbhandle.query(RecoveryType).filter(RecoveryType.name==rtn).first()
-			    cr = CampaignRecovery(campaign_definition_id = campaign_definition_id, recovery_order = i, recovery_type = rt)
-			    dbhandle.add(cr)
+				rt = dbhandle.query(RecoveryType).filter(RecoveryType.name==rtn).first()
+				cr = CampaignRecovery(campaign_definition_id = campaign_definition_id, recovery_order = i, recovery_type = rt)
+				dbhandle.add(cr)
 			dbhandle.commit()
-		    except IntegrityError, e:
-			message = "Integrity error - you are most likely using a name which already exists in database."
-			loghandle(e.message)
-			dbhandle.rollback()
-		    except SQLAlchemyError, e:
-			message = "SQLAlchemyError.  Please report this to the administrator.   Message: %s" % e.message
-			loghandle(e.message)
-			dbhandle.rollback()
-		    else:
-			dbhandle.commit()
+			except IntegrityError, e:
+				message = "Integrity error - you are most likely using a name which already exists in database."
+				loghandle(e.message)
+				dbhandle.rollback()
+			except SQLAlchemyError, e:
+				message = "SQLAlchemyError.  Please report this to the administrator.   Message: %s" % e.message
+				loghandle(e.message)
+				dbhandle.rollback()
+			else:
+				dbhandle.commit()
 
 		# Find definitions
 		if exp: # cuz the default is find
-		    data['curr_experiment'] = exp
-		    data['authorized'] = seshandle('experimenter').is_authorized(exp)
-		    # for testing ui...
-		    #data['authorized'] = True
-		    data['definitions'] = (dbhandle.query(CampaignDefinition,Experiment)
-					   .join(Experiment)
-					   .filter(CampaignDefinition.experiment==exp)
-					   .order_by(CampaignDefinition.name)
-					   )
-		    # Build the recoveries for each campaign.
-		    cids = [row[0].campaign_definition_id for row in data['definitions'].all()]
-		    recs_dict = {}
-		    for cid in cids:
+			data['curr_experiment'] = exp
+			data['authorized'] = seshandle('experimenter').is_authorized(exp)
+			# for testing ui...
+			#data['authorized'] = True
+			data['definitions'] = (dbhandle.query(CampaignDefinition,Experiment)
+									.join(Experiment)
+									.filter(CampaignDefinition.experiment==exp)
+									.order_by(CampaignDefinition.name)
+									)
+			# Build the recoveries for each campaign.
+			cids = [row[0].campaign_definition_id for row in data['definitions'].all()]
+			recs_dict = {}
+			for cid in cids:
 			recs = (dbhandle.query(CampaignRecovery).join(CampaignDefinition).options(joinedload(CampaignRecovery.recovery_type))
 				.filter(CampaignRecovery.campaign_definition_id == cid,CampaignDefinition.experiment == exp)
 				.order_by(CampaignRecovery.campaign_definition_id, CampaignRecovery.recovery_order))
-
 			rec_list  = []
 			for rec in recs:
-			    rec_list.append(rec.recovery_type.name )
+				rec_list.append(rec.recovery_type.name )
 			recs_dict[cid] = json.dumps(rec_list)
-		    data['recoveries'] = recs_dict
+			data['recoveries'] = recs_dict
+			data['rtypes'] = (dbhandle.query(RecoveryType.name,RecoveryType.description).order_by(RecoveryType.name).all())
 
-		    data['rtypes'] = (dbhandle.query(RecoveryType.name,RecoveryType.description).order_by(RecoveryType.name).all())
 		data['message'] = message
 		return data
 
@@ -182,67 +176,63 @@ class CampaignsPOMS():
 		data = {}
 		message = None
 		data['exp_selections'] = dbhandle.query(Experiment).filter(~Experiment.experiment.in_(["root","public"])).order_by(Experiment.experiment)
-
 		action = kwargs.pop('action',None)
 		exp = kwargs.pop('experiment',None)
-
 		if action == 'delete':
-		    campaign_id = kwargs.pop('campaign_id')
-		    name = kwargs.pop('name')
-		    try:
-			dbhandle.query(CampaignDependency).filter(or_(CampaignDependency.needs_camp_id==campaign_id,
+			campaign_id = kwargs.pop('campaign_id')
+			name = kwargs.pop('name')
+			try:
+				dbhandle.query(CampaignDependency).filter(or_(CampaignDependency.needs_camp_id==campaign_id,
 								CampaignDependency.uses_camp_id==campaign_id)).delete()
-			dbhandle.query(Campaign).filter(Campaign.campaign_id==campaign_id).delete()
-			dbhandle.commit()
-		    except Exception, e:
-			message = "The campaign, %s, has been used and may not be deleted." % name
-			loghandle(message)
-			loghandle(e.message)
-			dbhandle.rollback()
+				dbhandle.query(Campaign).filter(Campaign.campaign_id==campaign_id).delete()
+				dbhandle.commit()
+			except Exception, e:
+				message = "The campaign, %s, has been used and may not be deleted." % name
+				loghandle(message)
+				loghandle(e.message)
+				dbhandle.rollback()
 
 		if action == 'add' or action == 'edit':
-		    campaign_id = kwargs.pop('ae_campaign_id')
-		    name = kwargs.pop('ae_campaign_name')
-		    active = kwargs.pop('ae_active')
-		    split_type = kwargs.pop('ae_split_type')
-		    vo_role = kwargs.pop('ae_vo_role')
-		    software_version = kwargs.pop('ae_software_version')
-		    dataset = kwargs.pop('ae_dataset')
-		    param_overrides = kwargs.pop('ae_param_overrides')
-		    campaign_definition_id = kwargs.pop('ae_campaign_definition_id')
-		    launch_id = kwargs.pop('ae_launch_id')
-		    experimenter_id = kwargs.pop('experimenter_id')
-		    depends = kwargs.pop('ae_depends')
-		    if depends and depends != "[]":
-			depends = json.loads(depends)
-		    else:
-			depends = {"campaigns": [], "file_patterns": []}
-
-		    try:
-			if action == 'add':
-			    c = Campaign(name=name, experiment=exp,vo_role=vo_role,
-					 active=active, cs_split_type = split_type,
-					 software_version=software_version, dataset=dataset,
-					 param_overrides=param_overrides, launch_id=launch_id,
-					 campaign_definition_id=campaign_definition_id,
-					 creator=experimenter_id, created=datetime.now(utc))
-
-			    dbhandle.add(c)
-			    dbhandle.flush()
-			    campaign_id = c.campaign_id
+			campaign_id = kwargs.pop('ae_campaign_id')
+			name = kwargs.pop('ae_campaign_name')
+			active = kwargs.pop('ae_active')
+			split_type = kwargs.pop('ae_split_type')
+			vo_role = kwargs.pop('ae_vo_role')
+			software_version = kwargs.pop('ae_software_version')
+			dataset = kwargs.pop('ae_dataset')
+			param_overrides = kwargs.pop('ae_param_overrides')
+			campaign_definition_id = kwargs.pop('ae_campaign_definition_id')
+			launch_id = kwargs.pop('ae_launch_id')
+			experimenter_id = kwargs.pop('experimenter_id')
+			depends = kwargs.pop('ae_depends')
+			if depends and depends != "[]":
+				depends = json.loads(depends)
 			else:
-			    columns = {"name":                  name,
-				       "vo_role":               vo_role,
-				       "active":                active,
-				       "cs_split_type":         split_type,
-				       "software_version":      software_version,
-				       "dataset" :              dataset,
-				       "param_overrides":       param_overrides,
-				       "campaign_definition_id":campaign_definition_id,
-				       "launch_id":             launch_id,
-				       "updated":               datetime.now(utc),
-				       "updater":               experimenter_id
-				       }
+				depends = {"campaigns": [], "file_patterns": []}
+			try:
+				if action == 'add':
+					c = Campaign(name=name, experiment=exp,vo_role=vo_role,
+								active=active, cs_split_type = split_type,
+								software_version=software_version, dataset=dataset,
+								param_overrides=param_overrides, launch_id=launch_id,
+								campaign_definition_id=campaign_definition_id,
+								creator=experimenter_id, created=datetime.now(utc))
+					dbhandle.add(c)
+					dbhandle.flush() ##### Is this flush() necessary or better a commit ?
+					campaign_id = c.campaign_id
+			else:
+				columns = {"name":                  name,
+						"vo_role":               vo_role,
+						"active":                active,
+						"cs_split_type":         split_type,
+						"software_version":      software_version,
+						"dataset" :              dataset,
+						"param_overrides":       param_overrides,
+						"campaign_definition_id":campaign_definition_id,
+						"launch_id":             launch_id,
+						"updated":               datetime.now(utc),
+						"updater":               experimenter_id
+						} ######IM HERE PIPE
 			cd = dbhandle.query(Campaign).filter(Campaign.campaign_id==campaign_id).update(columns)
 			# now redo dependencies
 			dbhandle.query(CampaignDependency).filter(CampaignDependency.uses_camp_id == campaign_id).delete()
@@ -326,22 +316,3 @@ class CampaignsPOMS():
 		    definition['definition_parameters'] = cdef.definition_parameters
 		    data['definition'] = definition
 		return json.dumps(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
