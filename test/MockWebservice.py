8610dd285d1c52fdd7852172e5b31511a45dbec8
Marc Mengel <mengel@fnal.gov>
2017-01-17 19:17:57 -0600

    woo ho

--- test/MockWebservice.py
+++ test/MockWebservice.py
@@ -52,3 +52,15 @@ def testMockWebservice2():
      #print l
      assert(l[0] == "post_data = %s\n" % json.dumps(data))
      assert(l[1] == ('127.0.0.1 - - [%s] "POST /foo/bar HTTP/1.1" 200 -\n' % ts))
+
+def testMockWebservice3():
+     mw = MockWebservice()
+     import urllib2
+     ts = time.strftime("%d/%b/%Y %H:%M:%S")
+     r = urllib2.urlopen("http://127.0.0.1:8888/poms/active_jobs")
+     txt = r.read()
+     r.close()
+     assert(txt == "[]\n")
+     l = mw.log.readlines()
+     mw.close()
+     assert(l[0] == ('127.0.0.1 - - [%s] "GET /poms/active_jobs HTTP/1.1" 200 -\n' % ts))
