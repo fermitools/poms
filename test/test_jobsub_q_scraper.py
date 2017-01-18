b31b41a35ed63a59f3e7153ecb67d70628ec2435
Marc Mengel <mengel@fnal.gov>
2017-01-17 19:38:56 -0600

    testing jobsub_q_scraper now

--- test/test_jobsub_q_scraper.py
+++ test/test_jobsub_q_scraper.py
@@ -5,6 +5,8 @@ import subprocess
 import re
 import os
 import time
+import json
+import urllib
 
 class TestJobsub_q_scraper:
 
@@ -18,7 +20,24 @@ class TestJobsub_q_scraper:
         self.mw.close()
         self.mq.close()
 
+    def _parse_line(self,line):
+        res = {}
+        for aeqb in line.split(';'):
+            var,val = aeqb.split('=',1)
+            res[var] = val
+        return res
+
     def _do_test(self,fname):
+
+        self.map = {
+           "0": "Unexplained",
+           "1": "Idle",
+           "2": "Running",
+           "3": "Removed",
+           "4": "Completed",
+           "5": "Held",
+           "6": "Submission_error",
+        }
 
         print "_do_test: ", fname
         self.mq.setoutput(fname)
@@ -29,15 +48,22 @@ class TestJobsub_q_scraper:
         time.sleep(1)
 
         df = open("%s/test/data/%s" % (os.environ['POMS_DIR'],fname), "r")
+        active_log_line = self.mw.log.next()
 
         for line in df:
             data_log = self.mw.log.next()
             post_log = self.mw.log.next()
-            print "dl:", line
-            print "pd:", data_log
-            print "ll:", post_log
-            
+
+            line_data = self._parse_line(line)
+            post_data =  json.loads(data_log[12:-1])
+
+            # did we post what it said?
+
+            assert(post_data['jobsub_job_id'].replace('%40','@') == line_data['JOBSUBJOBID'])
+            assert(post_data['task_id'] == line_data['POMS_TASK_ID'])
+            assert(post_data['status'] == self.map[line_data['JOBSTATUS']])
+            assert(post_data['cpu_time'] == line_data['RemoteUserCpu'])
+
     def test_condor_q_out_1(self):
         self._do_test('condor_q_out1')
-        assert(False)
 
