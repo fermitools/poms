8526fbe388f88b316a84536a4666dbc637907253
ahandres <ahandres@fnal.gov>
2017-03-13 17:03:29 -0500

    space import

--- webservice/TagsPOMS.py
+++ webservice/TagsPOMS.py
@@ -9,6 +9,7 @@
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy import func, desc
 from model.poms_model import Service, ServiceDowntime, Experimenter, Experiment, ExperimentsExperimenters, Job, JobHistory, Task, CampaignDefinition, TaskHistory, Campaign, LaunchTemplate, Tag, CampaignsTags, JobFile, CampaignSnapshot, CampaignDefinitionSnapshot,LaunchTemplateSnapshot,CampaignRecovery,RecoveryType, CampaignDependency
+
 import json
 
 class TagsPOMS():
