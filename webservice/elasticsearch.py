0e8e161aaee4b2cd84b94b22ac29fb7994bc1042
Vladimir Podstavkov <podstvkv@fnal.gov>
2017-03-24 15:45:33 -0500

    Code cleanup

--- webservice/elasticsearch.py
+++ webservice/elasticsearch.py
@@ -2,31 +2,30 @@ import os
 import requests
 import pprint
 import json
-import cherrypy
 from datetime import datetime
-from utc import utc
 
-class Elasticsearch:
 
-    def __init__(self,  config = None ,debug=0):
-        if config == None:
+class Elasticsearch(object):
+
+    def __init__(self, config=None, debug=0):
+        if config is None:
             self.config = {
-                'elasticsearch_base_url':'file:///dev/null',
-                'elasticsearch_cert':'/dev/null',
-                'elasticsearch_key':'/dev/null',
+                'elasticsearch_base_url': 'file:///dev/null',
+                'elasticsearch_cert': '/dev/null',
+                'elasticsearch_key': '/dev/null',
             }
         else:
             self.config = config
-        if debug == 1 or self.config.get('elasticsearch_base_url') == None:
-            self.base_url="https://fifemon-es.fnal.gov"
+        if debug == 1 or self.config.get('elasticsearch_base_url') is None:
+            self.base_url = "https://fifemon-es.fnal.gov"
             #self.base_url="http://sammongpvm01.fnal.gov:9200"
         else:
-            self.base_url=self.config.get('elasticsearch_base_url').strip('"')
+            self.base_url = self.config.get('elasticsearch_base_url').strip('"')
 
         #configfile = "poms.ini"
-        #self.config.update(configfile) 
-        self.cert=self.config.get('elasticsearch_cert','').strip('"')
-        self.key=self.config.get('elasticsearch_key','').strip('"')
+        #self.config.update(configfile)
+        self.cert = self.config.get('elasticsearch_cert', '').strip('"')
+        self.key = self.config.get('elasticsearch_key', '').strip('"')
         if self.cert == "":
             self.cert = "/tmp/x509up_u%d" % os.getuid()
             self.key = self.cert
@@ -103,12 +102,12 @@ if __name__ == '__main__':
 
     #example of basic searching and getting particular fields from object
     query = {
-        'query' : {
-            'term' : { 'jobid' : '9034906.0@fifebatch1.fnal.gov' }
+        'query': {
+            'term': {'jobid': '9034906.0@fifebatch1.fnal.gov'}
         }
     }
 
-    response= es.search(index='fifebatch-logs-*', types=['condor_eventlog'], query=query)
+    response = es.search(index='fifebatch-logs-*', types=['condor_eventlog'], query=query)
     pprint.pprint(response)
 
     print "# of records: ", response.get('hits').get('total')
@@ -118,7 +117,7 @@ if __name__ == '__main__':
         print record.get('_source').get('event_message')
         print "*" * 13
 
-    print "*" * 80 
+    print "*" * 80
 
     #emample of searching by field
     query = {
@@ -130,54 +129,54 @@ if __name__ == '__main__':
     response = es.search(index='fifebatch-logs-*', types=['condor_eventlog'], query=query)
     pprint.pprint(response)
 
-    print "*" * 80 
+    print "*" * 80
 
     #example of iterating the result set
     query = {
-        'query' : {
-            'term' : { 'jobid' : '9034906.0@fifebatch1.fnal.gov' }
+        'query': {
+            'term': {'jobid': '9034906.0@fifebatch1.fnal.gov'}
         }
     }
 
     response = es.search(index='fifebatch-logs-*', types=['condor_eventlog'], query=query)
 
     for record in response.get('hits').get('hits'):
-        for k,v in record.get("_source").iteritems():
+        for k, v in record.get("_source").iteritems():
             print str(k) + ": " + str(v)
         print "---"
 
-    print "*" * 80 
+    print "*" * 80
 
     #example of specifying a from and a size which can be useful for pagination
     query = {
-        "from" : 0, "size" : 3,
-        'query' : {
-            'term' : { 'jobid' : '9034906.0@fifebatch1.fnal.gov' }
+        "from": 0, "size": 3,
+        'query': {
+            'term': {'jobid': '9034906.0@fifebatch1.fnal.gov'}
         }
     }
     response = es.search(index='fifebatch-logs-*', types=['condor_eventlog'], query=query)
     pprint.pprint(response)
 
-    print "*" * 80 
+    print "*" * 80
 
     #example of sorting by date
     query = {
-        "sort" : [{ "@timestamp" : {"order" : "asc"}}],
-        'query' : {
-            'term' : { 'jobid' : '9034906.0@fifebatch1.fnal.gov' }
+        "sort": [{"@timestamp": {"order": "asc"}}],
+        'query': {
+            'term': {'jobid': '9034906.0@fifebatch1.fnal.gov'}
         }
     }
     response = es.search(index='fifebatch-logs-*', types=['condor_eventlog'], query=query)
     pprint.pprint(response)
 
-    print "*" * 80 
+    print "*" * 80
 
     #example of sending a record where datetimes will be serialized
     payload = {'timestamp': datetime.utcnow(), 'message': 'trying out elasticsearch with poms', 'user': 'mengel'}
     response = es.index(index='poms-2016-09-02', doc_type='my-poms-type', body=payload)
     pprint.pprint(response)
 
-    print "*" * 80 
+    print "*" * 80
 
     #example of sending a record where datetimes will not be serialized
     payload = {'timestamp': '2016-09-02T20:00:00', 'message': 'trying out elasticsearch with poms not serialized', 'user': 'mengel'}
@@ -187,23 +186,22 @@ if __name__ == '__main__':
     print "= " * 40
 
     query = {
-        "sort" : [{ "@timestamp" : {"order" : "asc"}}],
-        "query" : {
-           "bool": {  
-              "must": {
-		    "range" : {
-		       'timestamp' : {
-			      'gte': "now-5m",
-			      'lte': "now", 
-			}
-		  }
-               },
-               "must": {
-                    "exists": { "field": "env.POMS_TASK_ID" }
-                
-              }
-          }
-       }
+        "sort": [{"@timestamp": {"order": "asc"}}],
+        "query": {
+            "bool": {
+                "must": {
+                    "range": {
+                        'timestamp': {
+                            'gte': "now-5m",
+                            'lte': "now",
+                        }
+                    }
+                },
+                "must": {
+                    "exists": {"field": "env.POMS_TASK_ID"}
+                }
+            }
+        }
     }
     #response = es.search(index='fifebatch-logs-*', types=['job'], query=query)
     response = es.search(index='fifebatch-jobs.*', type=['job'], query=query)
