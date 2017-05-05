da0c0c2fece2f55dee2cfb5ad46046434a71556f
Stephen P. White <swhite@fnal.gov>
2017-05-05 11:14:11 -0500

    Now creates optional log file.

--- bin/addusers.py
+++ bin/addusers.py
@@ -1,13 +1,17 @@
 #!/bin/env python
 
-import os, argparse, psycopg2, json
+import os, argparse, psycopg2, json, time
 
 dbg = False
+log = None
 
 def debug(message):
     global dbg
+    global log
     if dbg:
         print message
+    if log:
+        log.write(message+'\n')
 
 def parse_command_line():
     parser = argparse.ArgumentParser(description='Loads a VOMS generated JSON file of experimenters into POMS.')
@@ -17,7 +21,8 @@ def parse_command_line():
     parser.add_argument('user', help="User to connect as")
     parser.add_argument('file', help="File of users to read in.")
     parser.add_argument('-p', '--password', help="Password for the database user account. For testing only, for production use .pgpass .")
-    parser.add_argument('-d', '--debug', action="store_true", help="Debug")
+    parser.add_argument('-d', '--debug', action="store_true", help="Send debug data to screen")
+    parser.add_argument('-l', '--log', help="Directory/filename for logging debug data.")
     args = parser.parse_args()
     # Get password if not suppled
     #d = vars(args)
@@ -62,11 +67,16 @@ def add_relationship(cursor,experimenterid,exp):
 
 def main():
     global dbg
+    global log
     args = parse_command_line()
     if args.debug:
         dbg = True
+    if args.log:
+        logfile = "%s-%s.log" % (args.log,time.strftime('%Y%m%d%H%M%S'))
+        log = open(logfile,'w')
     if os.path.isfile(args.file) == False:
-        print "main: missing file: %s" % args.file
+        debug("main: missing file: %s" % args.file)
+        print "addusers.py -- main: missing file: %s" % args.file
         raise SystemExit
     else:
         json_data = open(args.file)
@@ -80,7 +90,7 @@ def main():
         cursor = conn.cursor()
         for exp in expdict.keys():
             if verify_exp(cursor,exp) == False:
-                print 'experiment: %s does not exist in poms' % exp
+                debug('experiment: %s does not exist in poms' % exp)
             else:
                 for user in expdict[exp]:
                     debug("main: processing <%s> <%s> <%s>" % (exp, user['username'], user['commonname']))
