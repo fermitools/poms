27a1aa77b19482d2ce3b6978e675e4973434487e
Marc Mengel <mengel@fnal.gov>
2017-05-11 16:38:45 -0500

    cache triage pages...

--- webservice/TriagePOMS.py
+++ webservice/TriagePOMS.py
@@ -13,6 +13,7 @@ from .elasticsearch import Elasticsearch
 from sqlalchemy import func, desc, not_, and_
 from collections import OrderedDict
 
+from .pomscache import pomscache
 
 class TriagePOMS(object):
 
@@ -53,6 +54,7 @@ class TriagePOMS(object):
         return out
 
 
+    @pomscache.cache_on_arguments()
     def triage_job(self, dbhandle, jobsub_fetcher, config, job_id, tmin=None, tmax=None, tdays=None, force_reload=False):
         # we don't really use these for anything but we might want to
         # pass them into a template to set time ranges...
