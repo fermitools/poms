f237b35ecca5b53d0fc5872ade3a7d9a95bfe990
Marc Mengel <mengel@fnal.gov>
2017-07-24 16:36:28 -0500

    sigh

--- webservice/pomscache.py
+++ webservice/pomscache.py
@@ -8,7 +8,7 @@ pomscache = make_region(
    arguments = {
       "filename": "/tmp/poms_dogpile_cache",
       "rw_lockfile": False,
-      "dogpile_lockfile": False,
+      # "dogpile_lockfile": False,
    }
 )
 
@@ -20,6 +20,6 @@ pomscache_10 = make_region(
    arguments = {
       "filename": "/tmp/poms_dogpile_cache_10",
       "rw_lockfile": False,
-      "dogpile_lockfile": False, # note this only coordinates within processes
+      # "dogpile_lockfile": False, # note this only coordinates within processes
    }
 )
