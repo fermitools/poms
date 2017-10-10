f573ffea88da5056d13057d5882d154c06d624d4
Marc Mengel <mengel@fnal.gov>
2017-10-10 14:38:06 -0500

    use logging...

--- job_broker/poms_client/python/poms_client.py
+++ job_broker/poms_client/python/poms_client.py
@@ -4,11 +4,17 @@ import sys
 import requests
 import os
 import json
+import logging
 try:
     import configparser as ConfigParser
 except:
     import ConfigParser
 
+try:
+    import requests.packages.urllib3 as urllib3
+    urllib3.disable_warnings()
+except:
+    pass
 
 rs = requests.Session()
 
@@ -53,19 +59,19 @@ def launch_template_edit(action = None, launch_name = None, launch_host = None, 
         ae_launch_setup=""
         for arg_setup in launch_setup:
                 ae_launch_setup= ae_launch_setup+str(arg_setup)+" "
-        print("The ae_launch_setup is: " +  str(ae_launch_setup))
+        logging.debug("The ae_launch_setup is: " +  str(ae_launch_setup))
     else:
         ae_launch_setup = launch_setup
 
     #pc_email = pc_email #no useing pc_username
 
     if experiment == None or pc_username == None:
-        print(" You should provide an experiment name and email")
+        logging.error(" You should provide an experiment name and email")
     else:
 
         if action == 'delete':
             if ae_launch_name == None:
-                print("For deleting you need to provide the name of the launch template as name = name_of_your_launch_template")
+                logging.error("For deleting you need to provide the name of the launch template as name = name_of_your_launch_template")
             else:
                 data, status_code = make_poms_call(
                     pcl_call=1,
@@ -75,11 +81,11 @@ def launch_template_edit(action = None, launch_name = None, launch_host = None, 
                     ae_launch_name = ae_launch_name,
                     experiment = experiment,
                     test_client=test_client)
-                return "status_code: ", status_code
+                return "status_code", status_code
 
         if action == 'add':
             if ae_launch_name == None or ae_launch_host == None or ae_launch_account == None or ae_launch_setup == None:
-                print("Your should provide the launch_name in order to add name, launch_host, user_account, launch_setup. \n\
+                logging.error("Your should provide the launch_name in order to add name, launch_host, user_account, launch_setup. \n\
                         Curently you provide launch_name="+str(ae_launch_name)+",launch_host="+str(ae_launch_host)+", user_account="+str(ae_launch_account)+", setup="+str(ae_launch_setup)+".")
             else:
                 data, status_code  = make_poms_call(
@@ -102,7 +108,7 @@ def launch_template_edit(action = None, launch_name = None, launch_host = None, 
 
         elif action == 'edit':
             if ae_launch_name == None:
-                print("Your should provide the launch_name in order to edit\n\
+                logging.error("Your should provide the launch_name in order to edit\n\
                     Curently you provide name = "+str(ae_launch_name))
             else:
                 data, status_code = make_poms_call(
@@ -117,14 +123,16 @@ def launch_template_edit(action = None, launch_name = None, launch_host = None, 
                     ae_launch_account = user_account,
                     ae_launch_setup = launch_setup,
                     test_client=test_client)
-                return "status_code=", status_code
+                return "status_code", status_code
                     ###The other var are query in the CampaignsPOMS module ae_launch_id, experimenter_id.
                 #return data['message']
 
         else:
-            print("You should define an action on your launch_template, there are just \
+            logging.error("You should define an action on your launch_template, there are just \
             three posibilities: action = add, action = edit or action = delete. You choose action = "+action+"\n \
             You did not change anything in your template")
+
+        return "failed", -1
 
 
 def campaign_definition_edit(output_file_patterns, launch_script,
@@ -143,7 +151,7 @@ def campaign_definition_edit(output_file_patterns, launch_script,
         ae_launch_script=""
         for arg_setup in launch_script:
                 ae_launch_script= ae_launch_script+str(arg_setup)+" "
-        print("The ae_launch_setup is: "+ str(ae_launch_setup))
+        logging.info("The ae_launch_setup is: "+ str(ae_launch_setup))
     else:
         ae_launch_script = launch_script
 
@@ -161,7 +169,7 @@ def campaign_definition_edit(output_file_patterns, launch_script,
                             ae_definition_parameters= ae_definition_parameters,
                             test_client=test_client
                             )
-    return "status_code =", status_code
+    return "status_code", status_code
 
     #return data['message']
 
@@ -171,8 +179,8 @@ def campaign_edit (action, ae_campaign_name, pc_username, experiment, vo_role,
                     ae_completion_type, ae_completion_pct, ae_param_overrides,
                     ae_depends, ae_launch_name, ae_campaign_definition, test_client):
     method="campaign_edit"
-    print("#"*10)
-    print(ae_param_overrides)
+    logging.debug("#"*10)
+    logging.debug(ae_param_overrides)
     if ae_param_overrides:
         param_temp=""
         for arg_param in ae_param_overrides:
@@ -181,14 +189,12 @@ def campaign_edit (action, ae_campaign_name, pc_username, experiment, vo_role,
         try:
             json.dumps(ae_param_overrides)
         except:
-            print("please use the right json format for the parameters")
-        '''
-        print("#"*10)
-        print("type" + type(ae_param_overrides))
-        print("The ae_param_overrides is: " +  ae_param_overrides)
-        '''
+            logging.error("please use the right json format for the parameters")
+        logging.debug("#"*10)
+        logging.debug("type" + type(ae_param_overrides))
+        logging.debug("The ae_param_overrides is: " +  ae_param_overrides)
     else:
-        print("conserving params, not override anything.")
+        logging.debug("conserving params, not override anything.")
     data, status_code = make_poms_call(pcl_call=1,
                             method=method,
                             action=action,
@@ -207,7 +213,7 @@ def campaign_edit (action, ae_campaign_name, pc_username, experiment, vo_role,
                             ae_launch_name=ae_launch_name,
                             ae_campaign_definition=ae_campaign_definition,
                             test_client=test_client)
-    return "status_code =", status_code
+    return "status_code", status_code
     #return data['message']
 
 
@@ -226,7 +232,7 @@ def auth_cert():
             if os.path.exists(proxypath):
                 cert=proxypath
         if not cert:
-            print("You should generate a proxy for use the client, you can use kx509 to generate your proxy. If you have a proxy please provide the location at the enviroment variable X509_USER_PROXY")
+            logging.error("You should generate a proxy for use the client, you can use kx509 to generate your proxy. If you have a proxy please provide the location at the enviroment variable X509_USER_PROXY")
         return cert
 
 def make_poms_call(**kwargs):
@@ -244,7 +250,7 @@ def make_poms_call(**kwargs):
     elif test_client:
         #base=config['url']['base_dev_ssl']
         base=config.get('url','base_dev_ssl')
-        print("base = " + base)
+        logging.debug("base = " + base)
     else:
         #base=config['url']['base_prod']
         base=config.get('url','base_prod')
@@ -254,25 +260,26 @@ def make_poms_call(**kwargs):
         if kwargs[k] == None:
             del kwargs[k]
     if os.environ.get("POMS_CLIENT_DEBUG", None):
-        sys.stderr.write("poms_client: making call %s( %s ) at %s with the proxypath = %s" % (method, kwargs, base, cert))
+        logging.debug("poms_client: making call %s( %s ) at %s with the proxypath = %s" % (method, kwargs, base, cert))
     cert=auth_cert()
     rs.cert=cert
     rs.key=cert
-    sys.stderr.write("poms_client: making call %s( %s ) at %s with the proxypath = %s" % (method, kwargs, base, cert))
+    logging.debug("poms_client: making call %s( %s ) at %s with the proxypath = %s" % (method, kwargs, base, cert))
     c = rs.post("%s/%s" % (base,method), data=kwargs, verify=False);
     res = c.text
     status_code = c.status_code
     c.close()
-    #print("\n\nres =" + str(res))
-    sys.stderr.write("status_code: "+ str(status_code))
+    #logging.debug("\n\nres =" + str(res))
+    logging.debug("status_code: "+ str(status_code))
     return res, status_code
     #return status_code
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     # simple tests...
     res = make_poms_call(test=True, method="active_jobs")
     tid = get_task_id_for(test = True, campaign=14, command_executed="fake test job")
     cid = register_poms_campaign("mwm_client_test",  user = "mengel", experiment = "samdev", version = "v0_0", dataset = "mwm_test_data", test = True)
-    print("got task id " +  str(tid))
-    print("got campaign id "+ str(cid))
+    logging.info("got task id " +  str(tid))
+    logging.info("got campaign id "+ str(cid))
