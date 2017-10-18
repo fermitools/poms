f6628a90c467b8893891512d0807587b1b644c01
Vladimir Podstavkov <podstvkv@fnal.gov>
2017-10-18 10:11:34 -0500

    Debug

--- webservice/TagsPOMS.py
+++ webservice/TagsPOMS.py
@@ -82,7 +82,13 @@ class TagsPOMS(object):
 
         cids = cl.split(',')        # Campaign IDs list
         # result = dbhandle.query(CampaignsTags.tag_obj.tag_name).filter(CampaignsTags.campaign_id.in_(cids)).distinct()
-        result = (dbhandle.query(Campaign.campaigns_tags)
+        #
+        # SELECT distinct(tags.tag_name)
+        # FROM campaigns_tags JOIN tags ON tags.tag_id=campaigns_tags.tag_id
+        # WHERE campaigns_tags.campaign_id in (513,514);
+        #
+        result = (dbhandle.query(Tags,CampaignsTags)
+                  .filter(Tags.tag_id == CampaignsTags.tag_id)
                   .filter(CampaignsTags.campaign_id.in_(cids)).all())
                   #.options(joinedload(CampaignsTags.task_obj))
         response = {"result": result, "msg": "OK"}
