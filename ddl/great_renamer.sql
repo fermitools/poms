d907649596ee2392b092cd38a0cfc287a5158a41
Marc Mengel <mengel@fnal.gov>
2018-05-30 16:20:10 -0500

    more complete sql renamer

--- ddl/great_renamer.sql
+++ ddl/great_renamer.sql
@@ -1,18 +1,45 @@
 
- ALTER TABLE launch_templates RENAME TO login_setups;
- ALTER TABLE login_setups RENAME COLUMN launch_template_id TO login_setup_id;
- ALTER TABLE campaign_definitions RENAME TO job_types;
- ALTER TABLE job_types RENAME COLUMN campaign_definition_id TO job_type_id;
- ALTER TABLE campaign_snapshots RENAME TO campaign_stage_snapshots;
- ALTER TABLE campaign_stage_snapshots RENAME COLUMN campaign_snapshot_id TO campaign_stage_snapshot_id;
- ALTER TABLE campaign_definition_snapshots RENAME TO job_type_snapshots;
- ALTER TABLE job_type_snapshots RENAME COLUMN campaign_definition_snap_id  TO job_type_snapshot_id;
- ALTER TABLE campaigns RENAME TO campaign_stages;
- ALTER TABLE campaign_stages RENAME COLUMN campaign_id to campaign_stage_id;
- ALTER TABLE tasks RENAME to submissions;
- ALTER TABLE submissions RENAME COLUMN task_id TO submission_id;
- ALTER TABLE submissions RENAME COLUMN task_params TO submission_params;
- ALTER TABLE tags RENAME TO campaigns;
- ALTER TABLE campaigns RENAME COLUMN tag_id to campaign_id;
- ALTER TABLE campaigns_tags RENAME TO campaigns_campaign_stages;
+ALTER TABLE launch_templates          RENAME COLUMN launch_id TO login_setup_id;
+ALTER TABLE campaigns                 RENAME COLUMN launch_id TO login_setup_id;
+ALTER TABLE campaign_snapshots        RENAME COLUMN launch_id TO login_setup_id;
+ALTER TABLE launch_template_snapshots RENAME COLUMN launch_id TO login_setup_id;
 
+
+ALTER TABLE campaign_definitions RENAME COLUMN campaign_definition_id TO job_type_id;
+ALTER TABLE campaigns            RENAME COLUMN campaign_definition_id TO job_type_id;
+ALTER TABLE campaign_snapshots   RENAME COLUMN campaign_definition_id TO job_type_id;
+ALTER TABLE campaign_recoveries  RENAME COLUMN campaign_definition_id TO job_type_id;
+
+
+ALTER TABLE campaign_snapshots RENAME COLUMN campaign_snapshot_id TO campaign_stage_snapshot_id;
+ALTER TABLE tasks              RENAME COLUMN campaign_snapshot_id TO campaign_stage_snapshot_id;
+
+ALTER TABLE campaign_definition_snapshots RENAME COLUMN campaign_definition_snap_id  TO job_type_snapshot_id;
+ALTER TABLE tasks                         RENAME COLUMN campaign_definition_snap_id  TO job_type_snapshot_id;
+
+ALTER TABLE campaigns           RENAME COLUMN campaign_id to campaign_stage_id;
+ALTER TABLE tasks               RENAME COLUMN campaign_id to campaign_stage_id;
+ALTER TABLE campaigns_tags      RENAME COLUMN campaign_id to campaign_stage_id;
+ALTER TABLE campaign_snapshots  RENAME COLUMN campaign_id to campaign_stage_id;
+ALTER TABLE held_launches       RENAME COLUMN campaign_id to campaign_stage_id;
+ALTER TABLE campaign_dependency RENAME COLUMN needs_camp_id to needs_campaign_stage_id;
+ALTER TABLE campaign_dependency RENAME COLUMN uses_camp_id to provides_campaign_stage_id;
+
+ALTER TABLE tags           RENAME COLUMN tag_id to campaign_id;
+ALTER TABLE campaigns_tags RENAME COLUMN tag_id to campaign_id;
+
+ALTER TABLE tasks                   RENAME COLUMN task_id TO submission_id;
+ALTER TABLE jobs                    RENAME COLUMN task_id TO submission_id;
+ALTER TABLE task_histories          RENAME COLUMN task_id TO submission_id;
+
+ALTER TABLE held_launches           RENAME column parent_task_id TO parent_submission_id;
+ALTER TABLE tasks                   RENAME COLUMN task_params TO submission_params;
+
+ALTER TABLE launch_templates              RENAME TO login_setups;
+ALTER TABLE campaign_definitions          RENAME TO job_types;
+ALTER TABLE campaign_snapshots            RENAME TO campaign_stage_snapshots;
+ALTER TABLE campaign_definition_snapshots RENAME TO job_type_snapshots;
+ALTER TABLE campaigns                     RENAME TO campaign_stages;
+ALTER TABLE tasks                         RENAME to submissions;
+ALTER TABLE tags                          RENAME TO campaigns;
+ALTER TABLE campaigns_tags                RENAME TO campaigns_campaign_stages;
--- webservice/great_renamer.pl
+++ webservice/great_renamer.pl
@@ -9,48 +9,37 @@ sub rename_em {
     $_ = $_[0];
 
     s/\bLaunchTemplate\b/LoginSetup/go;
-    s/\blaunch_template_id\b/login_setup_id/go;
+    s/\blaunch_id\b/login_setup_id/go;
     s/\blaunch_definitions\b/login_setups/go;
-    # ALTER TABLE launch_templates RENAME TO login_setups;
-    # ALTER TABLE login_setups RENAME COLUMN launch_template_id to login_setup_id;
 
     s/\bCampaignDefinition\b/JobType/go;
     s/\bcampaign_definition_id\b/job_type_id/go;
     s/\bcampaign_definitions\b/job_types/go;
-    # ALTER TABLE campaign_definitions RENAME TO job_types;
-    # ALTER TABLE job_types RENAME COLUMN campaign_definition_id TO job_type_id;
     s/\bcampaign_definition_obj\b/job_type_obj/go;
 
     s/\bCampaignSnapshot\b/CampaignStageSnapshot/go;
     s/\bcampaign_snapshot_id\b/campaign_stage_snapshot_id/go;
     s/\bcampaign_snapshots\b/campaign_stage_snapshots/go;
-    # ALTER TABLE campaign_snapshots RENAME TO campaign_stage_snapshots;
-    # ALTER TABLE campaign_stage_snapshots RENAME COLUMN campaign_snapshot_id TO campaign_stage_snapshot_id;
     s/\bcampaign_snap_obj\b/campaign_stage_snapshot_obj/go;
 
     s/\bCampaignDefinitionSnapshot\b/JobTypeSnapshot/go;
     s/\bcampaign_definition_snap_id\b/job_type_snapshot_id/go;
     s/\bcampaign_definition_snapshots\b/job_type_snapshots/go;
-    # ALTER TABLE campaign_definition_snapshots RENAME TO job_type_snapshots;
-    # ALTER TABLE job_type_snapshots RENAME COLUMN campaign_definition_snap_id  TO job_type_snapshot_id;
     s/\bcampaign_definition_snap_obj\b/job_type_snapshot_obj/go;
 
     s/\bCampaign\b/CampaignStage/go;
     s/\bcampaigns\b/campaign_stages/go;
     s/\bcampaign_id\b/campaign_stage_id/go;
-    # ALTER TABLE campaigns RENAME TO campaign_stages;
-    # ALTER TABLE campaign_stages RENAME COLUMN campaign_id to campaign_stage_id;
+    s/\bneeds_camp_id\b/needs_campaign_stage_id/go;
+    s/\buses_camp_id\b/provides_campaign_stage_id/go;
     s/\bcampaign_obj\b/campaign_stage_obj/go;
     s/\bc\b/cs/go;
 
     s/\bTask\b/Submission/go;
     s/\btasks\b/submissions/go;
     s/\btask_id\b/submission_id/go;
-    # ALTER TABLE tasks RENAME to submissions;
-    # ALTER TABLE submissions RENAME COLUMN task_id TO submission_id;
     s/\btask_obj\b/submission_obj/go;
     s/\btask_params\b/submission_params/go;
-    # ALTER TABLE submissions RENAME COLUMN task_params TO submission_params;
     s/\bt\b/s/go;
     s/\btid\b/sid/go;
 
@@ -58,12 +47,9 @@ sub rename_em {
     s/\bTag\b/Campaign/go;
     s/\btags\b/campaigns/go;
     s/\btag_id\b/campaign_id/go;
-    # ALTER TABLE tags RENAME TO campaigns;
-    # ALTER TABLE campaigns RENAME COLUMN tag_id to campaign_id;
 
     s/\bCampaignsTags\b/CampaignsCampaignStages/go;
     s/\bcampaigns_tags\b/campaigns_campaign_stages/go;
-    # ALTER TABLE campaigns_tags RENAME TO campaigns_campaign_stages;
 
     return $_;
 }
