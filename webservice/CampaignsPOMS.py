798a4e49d9b3ff2f5312b604dc245b591f74a386
Vladimir Podstavkov <podstvkv@fnal.gov>
2018-11-20 14:25:17 -0600

    Add experiment knowledge to campaign_time_bars method

--- webservice/CampaignsPOMS.py
+++ webservice/CampaignsPOMS.py
@@ -1444,19 +1444,19 @@ class CampaignsPOMS:
 
 
     # @pomscache_10.cache_on_arguments()
-    def campaign_time_bars(self, dbhandle, campaign_stage_id=None, campaign=None, tmin=None, tmax=None, tdays=1):
+    def campaign_time_bars(self, dbhandle, experimenter, campaign_stage_id=None, campaign=None, tmin=None, tmax=None, tdays=1):
         """
             Give time-bars for Tasks for this campaign in a time window
             using the time_grid code
         """
-        if campaign_stage_id == None:
+        if campaign_stage_id is None:
             base_link = 'campaign_time_bars?campaign={}&'.format(campaign)
         else:
             base_link = 'campaign_time_bars?campaign_stage_id={}&'.format(campaign_stage_id)
 
         (
             tmin, tmax, tmins, tmaxs, nextlink, prevlink, time_range_string, tdays
-        ) = self.poms_service.utilsPOMS.handle_dates(tmin, tmax, tdays,base_link)
+        ) = self.poms_service.utilsPOMS.handle_dates(tmin, tmax, tdays, base_link)
         tg = time_grid.time_grid()
         key = tg.key()
 
@@ -1467,14 +1467,17 @@ class CampaignsPOMS:
         sl = deque()
 
         if campaign_stage_id is not None:
-            icampaign_id = int(campaign_stage_id)
-            q = dbhandle.query(CampaignStage).filter(CampaignStage.campaign_stage_id == icampaign_id)
+            icampaign_stage_id = int(campaign_stage_id)
+            q = dbhandle.query(CampaignStage).filter(CampaignStage.campaign_stage_id == icampaign_stage_id)
             cpl = q.all()
             name = cpl[0].name
-        elif campaign is not None and campaign != "":
-            q = dbhandle.query(CampaignStage).join(Campaign).filter(
-                CampaignStage.campaign_id == Campaign.campaign_id,
-                Campaign.name == campaign)
+        elif campaign not in (None, ""):
+            q = (dbhandle.query(CampaignStage).join(Campaign)
+                 .filter(CampaignStage.campaign_id == Campaign.campaign_id,
+                         Campaign.name == campaign,
+                         Campaign.experiment == experimenter.session_experiment
+                         )
+                 )
             cpl = q.all()
             name = campaign
         else:
@@ -1491,14 +1494,17 @@ class CampaignsPOMS:
               .filter(Submission.campaign_stage_id.in_(cidl),
                       SubmissionHistory.submission_id == Submission.submission_id,
                       or_(and_(Submission.created > tmin, Submission.created < tmax),
-                          and_(Submission.updated > tmin,
-                               Submission.updated < tmax))).order_by(SubmissionHistory.submission_id,
-                                                                     SubmissionHistory.created).all())
+                          and_(Submission.updated > tmin, Submission.updated < tmax))
+                      )
+              .order_by(SubmissionHistory.submission_id, SubmissionHistory.created)
+              .all())
         items = deque()
         extramap = OrderedDict()
 
-        url_template_plain= "https://fifemon.fnal.gov/monitor/d/000000115/job-cluster-summary?var-cluster=%(jobsub_cluster)s&var-schedd=%(jobsub_schedd)s&from=%(tminsec)s000&to=now&refresh=3m&orgId=1"
-        url_template_dag= "https://fifemon.fnal.gov/monitor/d/000000188/dag-cluster-summary?var-cluster=%(jobsub_cluster)s&var-schedd=%(jobsub_schedd)s&from=%(tminsec)s000&to=now&refresh=3m&orgId=1"
+        url_template_plain = ("https://fifemon.fnal.gov/monitor/d/000000115/job-cluster-summary?"
+                              "var-cluster=%(jobsub_cluster)s&var-schedd=%(jobsub_schedd)s&from=%(tminsec)s000&to=now&refresh=3m&orgId=1")
+        url_template_dag = ("https://fifemon.fnal.gov/monitor/d/000000188/dag-cluster-summary?"
+                            "var-cluster=%(jobsub_cluster)s&var-schedd=%(jobsub_schedd)s&from=%(tminsec)s000&to=now&refresh=3m&orgId=1")
 
         failedlaunch_url_template = self.poms_service.path + "/list_launch_file?campaign_stage_id=%(campaign_stage_id)s&fname=%(created_s)s_%(creator)s"
 
@@ -1549,8 +1555,7 @@ class CampaignsPOMS:
                 items[-1].url = url_template_plain % items[-1].__dict__
 
         logit.log("campaign_time_bars: items: " + repr(items))
-        blob = tg.render_query_blob(tmin, tmax, items, 'jobsub_job_id',
-                                    extramap=extramap)
+        blob = tg.render_query_blob(tmin, tmax, items, 'jobsub_job_id', extramap=extramap)
         return "", blob, name, str(tmin)[:16], str(tmax)[:16], nextlink, prevlink, tdays, key, extramap
 
 
