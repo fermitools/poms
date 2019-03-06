a66307d8344e8169a7139661ee5dfafae1481295
mengel <mengel@fnal.gov>
2019-03-06 15:14:58 -0600

    ..but use the right exception

--- webservice/poms_service.py
+++ webservice/poms_service.py
@@ -38,7 +38,7 @@ from sqlalchemy.inspection import inspect
 import cherrypy
 from jinja2 import Environment, PackageLoader
 import jinja2.exceptions
-import psycopg2
+import sqlalchemy.exc
 import logging
 
 # we import our logic modules, so we can attach an instance each to
@@ -121,7 +121,7 @@ def error_rewrite(f):
         except KeyError as e:
             logging.exception("rewriting:")
             raise cherrypy.HTTPError(400, "Missing form field: %s" % repr(e))
-        except psycopg2.DataError as e:
+        except sqlalchemy.exc.DataError as e:
             logging.exception("rewriting:")
             raise cherrypy.HTTPError(400, "Invalid argument: %s" % repr(e))
         except ValueError as e:
