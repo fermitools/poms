8e6e193e9bc1c24dafb286d80107aac7330c8d81
Marc Mengel <mengel@fnal.gov>
2019-03-21 15:41:00 -0500

    new file

---
+++ webservice/get_user.py
@@ -0,0 +1,8 @@
+import cherrypy
+
+def get_user():
+    if cherrypy.request.headers.get('X-Shib-Userid', None):
+            username = cherrypy.request.headers['X-Shib-Userid']
+    elif cherrypy.config.get('standalone_test_user', None):
+            username = cherrypy.config.get('standalone_test_user', None)
+    return username
