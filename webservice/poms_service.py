58ee2ad24a4027f9c58204a8ae53be4efbca22c0
Marc Mengel <mengel@fnal.gov>
2019-04-02 09:47:14 -0500

    sample fixup round 1

--- webservice/poms_service.py
+++ webservice/poms_service.py
@@ -528,6 +528,10 @@ class PomsService:
     @cherrypy.expose
     @logit.logstartstop
     def sample_workflows(self, *args, **kwargs):
+
+        import mimetypes
+        mimetypes.types_map['.ini'] = 'text/plain'
+
         sl = [
             x.replace(os.environ['POMS_DIR'] + '/webservice/static/', '')
             for x in glob.glob(os.environ['POMS_DIR'] + '/webservice/static/samples/*')
