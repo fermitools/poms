e9fa28120c44ddef70b5a303a02d6517be06c00f
mengel <mengel@fnal.gov>
2019-04-16 13:44:40 -0500

    sigh

--- webservice/TaskPOMS.py
+++ webservice/TaskPOMS.py
@@ -91,6 +91,9 @@ class TaskPOMS:
         self.status_Completed = (
             dbhandle.query(SubmissionStatus.status_id).filter(SubmissionStatus.status == "Completed").first()[0]
         )
+        self.status_Removed = (
+            dbhandle.query(SubmissionStatus.status_id).filter(SubmissionStatus.status == "Removed").first()[0]
+        )
         self.status_New = dbhandle.query(SubmissionStatus.status_id).filter(SubmissionStatus.status == "New").first()[0]
         self.init_status_done = True
 
