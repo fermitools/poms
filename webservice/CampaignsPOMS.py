7ba6b4f370b5f19b14d82c5676b693874d2ea6aa
mengel <mengel@fnal.gov>
2019-06-28 14:36:39 -0500

    now actually just go there

--- webservice/CampaignsPOMS.py
+++ webservice/CampaignsPOMS.py
@@ -475,10 +475,10 @@ class CampaignsPOMS:
         res.append("var network = new vis.Network(container, data, options);")
         res.append("var dests=[")
         for c_s in csl:
-            res.append("  'https://%s/campaign_stage_info/%s/%s?campaign_id=%s'," % (
+            res.append("  '%s/campaign_stage_info/%s/%s?campaign_stage_id=%s'," % (
                 self.poms_service.path, ctx.experiment, ctx.role,  c_s.campaign_stage_id))
         res.append("];")
-        res.append("network.on('click', function(params) { if (!params or !params['node']{ return; } ;  alert('got: node' +JSON.stringify(params['node'][0])); alert(' should go to: ' + dests[params['node'][0]])})")
+        res.append("network.on('click', function(params) { if (!params || !params['nodes']){ return; } ; document.location = dests[params['nodes'][0]];})")
         res.append("</script>")
         return "\n".join(res)
 
