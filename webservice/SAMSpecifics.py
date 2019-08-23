23988defc8592789b2918b444b4165d2445e3d5a
mengel <mengel@fnal.gov>
2019-08-23 10:29:30 -0500

    missing paren in dimension

--- webservice/SAMSpecifics.py
+++ webservice/SAMSpecifics.py
@@ -158,7 +158,7 @@ class sam_specifics:
             base_dim_list.append(basedims)
 
             isparentof = "isparentof: " * s.campaign_stage_obj.output_ancestor_depth
-            ischildof = "ischildof: " * s.campaign_stage_obj.output_ancestor_depth
+            ischildof = "ischildof:(" * s.campaign_stage_obj.output_ancestor_depth
             isclose = ")" * s.campaign_stage_obj.output_ancestor_depth
 
             somekiddims = "%s and %s version %s %s" % (
