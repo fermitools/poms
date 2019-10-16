b42ae9972210bae48165c01c1a0726db44472dbc
mengel <mengel@fnal.gov>
2019-10-15 20:12:58 -0500

    getting the isdigit right for ints vs strings

--- webservice/Permissions.py
+++ webservice/Permissions.py
@@ -84,7 +84,7 @@ class Permissions:
             q = ctx.db.query(CampaignStage.experiment, Experimenter.username, CampaignStage.creator_role).join(  #
                 Experimenter, CampaignStage.creator == Experimenter.experimenter_id
             )
-            if item_id and item_id.isdigit():
+            if item_id  and str(item_id).isdigit():
                 k = "cs:%s" % item_id
                 q = q.filter(CampaignStage.campaign_stage_id == item_id)
             if name and campaign_id:
@@ -106,7 +106,7 @@ class Permissions:
             q = ctx.db.query(Campaign.experiment, Experimenter.username, Campaign.creator_role).join(  #
                 Experimenter, Campaign.creator == Experimenter.experimenter_id
             )
-            if item_id and not isinstance(item_id,list) and item_id.isdigit():
+            if item_id and str(item_id).isdigit():
                 k = "c:%s" % item_id
                 q = q.filter(Campaign.campaign_id == item_id)
             if name:
