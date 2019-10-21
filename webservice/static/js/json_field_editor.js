a6ef21504daf511b8b9576d9cfc0c2ffc8b148e2
Marc Mengel <mengel@fnal.gov>
2019-10-21 15:54:43 -0500

    get all the places with the wrong replace

--- webservice/static/js/json_field_editor.js
+++ webservice/static/js/json_field_editor.js
@@ -365,12 +365,12 @@ json_field_editor.dictsave = function (fid) {
     }
     id = fid.replace('edit_form_', '');
     dest = document.getElementById(id);
-    dest.value = JSON.stringify(res).replace(/([,])/g, '$1 '); // To comply with Python json.dumps() format
+    dest.value = JSON.stringify(res).replace(/","/g,'", "');      // To comply with Python json.dumps() format
     /* also update xxx_text if there is one */
     console.log(["updating", id, dest, res]);
     dest = document.getElementById(id + '_text');
     if (dest) {
-        dest.value = JSON.stringify(res).replace(/([,])/g, '$1 '); // To comply with Python json.dumps() format
+        dest.value = JSON.stringify(res).replace(/","/g,'", "');      // To comply with Python json.dumps() format
         console.log(["also updating", id + '_text', dest, res]);
     }
     json_field_editor.cancel(fid);
