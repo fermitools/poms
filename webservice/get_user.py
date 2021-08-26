38eb2b8e22e1bed11eb73c7e62c58c0badd48708
mengel <mengel@fnal.gov>
2021-08-26 20:37:44 +0000

    username from scitoken subject

--- webservice/get_user.py
+++ webservice/get_user.py
@@ -4,6 +4,8 @@ import cherrypy
 def get_user():
     if cherrypy.request.headers.get("X-Shib-Userid", None):
         username = cherrypy.request.headers["X-Shib-Userid"]
+    if cherrypy.request.headers.get("X-SCITOKEN_SUB", None):
+        username = cherrypy.request.headers["X-SCITOKEN_SUB"].replace("@fnal.gov","")
     elif cherrypy.config.get("standalone_test_user", None):
         username = cherrypy.config.get("standalone_test_user", None)
     return username
