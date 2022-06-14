ca2d3b315e9aadc55c6125b083009c0d45e67514
mengel <mengel@fnal.gov>
2022-06-14 19:44:25 +0000

    try to get username from scitoken scope

--- webservice/get_user.py
+++ webservice/get_user.py
@@ -5,8 +5,13 @@ def get_user():
     if cherrypy.request.headers.get("X-Shib-Userid", None) and cherrypy.request.headers["X-Shib-Userid"] != '(null)':
         username = cherrypy.request.headers["X-Shib-Userid"]
         
-    elif cherrypy.request.headers.get("X-Scitoken-Sub", None) and cherrypy.request.headers["X-Scitoken-Sub"] != '(null)':
-        username = cherrypy.request.headers["X-Scitoken-Sub"].replace("@fnal.gov","")
+    elif cherrypy.request.headers.get("X-Scitoken-Scope", None) and cherrypy.request.headers["X-Scitoken-Scope"] != '(null)':
+        # we have the username in the storage scope, so mooch it out of there
+        m = re.match(r".*storage.create:/\S*/users/(\S*)\s.*", cherrypy.request.headers["X-Scitoken-Scope"])
+        if m:
+            username = m.group(1)
+        else:
+            username = cherrypy.config.get("standalone_test_user", None)
     elif cherrypy.config.get("standalone_test_user", None):
         username = cherrypy.config.get("standalone_test_user", None)
     return username
